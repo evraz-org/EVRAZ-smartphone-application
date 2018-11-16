cc3da72c6caacd302120e230bc77da3c579591e4
Obrazcoff <obrazcoff@gmail.com>
2018-11-16 04:01:46 +0100

    Minor refactoring changes

--- app/src/main/java/com/bitshares/bitshareswallet/SettingsFragment.java
+++ app/src/main/java/com/bitshares/bitshareswallet/SettingsFragment.java
@@ -4,13 +4,8 @@ package com.bitshares.bitshareswallet;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.support.v7.preference.Preference;
 import android.support.v7.preference.PreferenceFragmentCompat;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-
 import org.evrazcoin.evrazwallet.R;
 
 
